474097cd8c5b066059b88515e5aa857285565fec
Curtis-Wong <66400282+Curtis-Wong@users.noreply.github.com>
2021-08-04 22:10:12 -0400

    Update database.py

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 #SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://aihsiumy_username:aihsiumy_username@localhost:3306/aihsiumy_question"
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:123456789@localhost:3306/studyApp"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:123@db:3306/studyApp"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:romeooscar2tango@localhost:3306/studyApp"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
