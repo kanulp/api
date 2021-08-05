c76154fe0fe33da265ac9ca7bcb76ceedcc653b6
Curtis-Wong <66400282+Curtis-Wong@users.noreply.github.com>
2021-08-04 22:11:21 -0400

    Update models.py

--- sql_app/models.py
+++ sql_app/models.py
@@ -7,35 +7,35 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True)
-    teacher = Column(String)
+    email = Column(String(255), unique=True)
+    teacher = Column(String(255))
     timestamp = Column(DateTime)
-    courseName = Column(String)
+    courseName = Column(String(255))
     incorrectCount = Column(Integer)
     correctCount = Column(Integer)
     answerCount = Column(Integer)
     setCount = Column(Integer)
     userAverage = Column(Integer)
     totalTime = Column(Integer)
-    eligible = Column(String)
+    eligible = Column(String(255))
 
 class UserName(Base):
     __tablename__ = "username"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True)
-    userName = Column(String, unique=True)
+    email = Column(String(255), unique=True)
+    userName = Column(String(255), unique=True)
 
 class CSV(Base):
     __tablename__ = "csv_logs"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True)
+    email = Column(String(255), unique=True)
     timestamp = Column(DateTime)
-    courseName = Column(String, unique=True)
-    weekstoDraw = Column(String)
-    minAns = Column(String)
-    minCorrect = Column(String)
-    minAverage = Column(String)
-    minSets = Column(String)
-    drawPrize = Column(String)
+    courseName = Column(String(255), unique=True)
+    weekstoDraw = Column(String(255))
+    minAns = Column(String(255))
+    minCorrect = Column(String(255))
+    minAverage = Column(String(255))
+    minSets = Column(String(255))
+    drawPrize = Column(String(255))
